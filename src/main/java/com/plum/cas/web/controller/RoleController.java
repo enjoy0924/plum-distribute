7536685aa333337547617b14c8a8ef09a7d44705
enjoy0924 <enjoy0924@163.com>
2015-09-08 21:18:13 +0800

    [ADD]second commit

---
+++ src/main/java/com/plum/cas/web/controller/RoleController.java
@@ -0,0 +1,89 @@
+package com.plum.cas.web.controller;
+
+
+import com.plum.cas.dto.Role;
+import com.plum.cas.service.ResourceService;
+import com.plum.cas.service.RoleService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ *
+ */
+@Controller
+@RequestMapping("/role")
+public class RoleController {
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private ResourceService resourceService;
+
+    @RequiresPermissions("role:view")
+    @RequestMapping(method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("roleList", roleService.findAll());
+        return "role/list";
+    }
+
+    @RequiresPermissions("role:create")
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String showCreateForm(Model model) {
+        setCommonData(model);
+        model.addAttribute("role", new Role());
+        model.addAttribute("op", "新增");
+        return "role/edit";
+    }
+
+    @RequiresPermissions("role:create")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String create(Role role, RedirectAttributes redirectAttributes) {
+        roleService.create(role);
+        redirectAttributes.addFlashAttribute("msg", "新增成功");
+        return "redirect:/role";
+    }
+
+    @RequiresPermissions("role:update")
+    @RequestMapping(value = "/{id}/update", method = RequestMethod.GET)
+    public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+        setCommonData(model);
+        model.addAttribute("role", roleService.findOne(id));
+        model.addAttribute("op", "修改");
+        return "role/edit";
+    }
+
+    @RequiresPermissions("role:update")
+    @RequestMapping(value = "/{id}/update", method = RequestMethod.POST)
+    public String update(Role role, RedirectAttributes redirectAttributes) {
+        roleService.modify(role);
+        redirectAttributes.addFlashAttribute("msg", "修改成功");
+        return "redirect:/role";
+    }
+
+    @RequiresPermissions("role:delete")
+    @RequestMapping(value = "/{id}/delete", method = RequestMethod.GET)
+    public String showDeleteForm(@PathVariable("id") Long id, Model model) {
+        setCommonData(model);
+        model.addAttribute("role", roleService.findOne(id));
+        model.addAttribute("op", "删除");
+        return "role/edit";
+    }
+
+    @RequiresPermissions("role:delete")
+    @RequestMapping(value = "/{id}/delete", method = RequestMethod.POST)
+    public String delete(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
+        roleService.delete(id);
+        redirectAttributes.addFlashAttribute("msg", "删除成功");
+        return "redirect:/role";
+    }
+
+    private void setCommonData(Model model) {
+        model.addAttribute("resourceList", resourceService.findAll());
+    }
+
+}
---
+++ src/main/java/com/plum/core/tenant/TenantAdmin.java
@@ -0,0 +1,7 @@
+package com.plum.core.tenant;
+
+/**
+ * Created by Andy on 2015/9/8.
+ */
+public class TenantAdmin {
+}
